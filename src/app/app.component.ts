e213ca8d9b34747d9bed2bb3aa22d15334572f13
Vasyl Kuznyak <vasyl.kuznyak@gmail.com>
2022-05-11 18:56:11 +0300

    done homework 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {User} from "./models/User";
+
+import {UserService} from "./services/user.service";
+import {PostService} from "./services/post.service";
+import {Post} from "./models/Post";
+import {Comments} from "./models/Comments";
+import {CommentsService} from "./services/comments.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'angular-dec2021';
+  users: User[];
+  posts: Post[];
+  comments: Comments[];
+
+  constructor(
+    private UserService: UserService,
+    private PostService: PostService,
+    private CommentsService: CommentsService) {
+
+    UserService.getAll().subscribe(users => this.users = users);
+    PostService.getAll().subscribe(posts => this.posts = posts);
+    CommentsService.getAll().subscribe(comments => this.comments = comments);
+  }
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {HttpClientModule} from "@angular/common/http";
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {UsersComponent} from './users/users.component';
+import {PostsComponent} from './posts/posts.component';
+import {CommentsComponent} from './comments/comments.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    PostsComponent,
+    CommentsComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
