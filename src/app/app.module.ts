83b6df61e39c20fdb2b721b9a65c6556042c025d
Vasyl Kuznyak <vasyl.kuznyak@gmail.com>
2022-05-12 13:34:27 +0300

    fixed homework1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,13 @@ import {BrowserModule} from '@angular/platform-browser';
 import {HttpClientModule} from "@angular/common/http";
 
 import {AppComponent} from "./app.component";
+import {UserComponent} from "./components/user/user.component";
+import {UsersComponent} from "./components/users/users.component";
+import {PostComponent} from "./components/post/post.component";
+import {PostsComponent} from "./components/posts/posts.component";
+import {CommentsComponent} from "./components/comments/comments.component";
+import {CommentComponent} from "./components/comment/comment.component";
 
-import {
-  CommentComponent,
-  CommentsComponent,
-  PostComponent,
-  PostsComponent,
-  UserComponent,
-  UsersComponent
-} from './components';
 
 @NgModule({
   declarations: [
@@ -31,4 +29,5 @@ import {
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule {
+}
