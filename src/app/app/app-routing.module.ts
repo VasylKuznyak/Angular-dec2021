9821b90a8da11c44b7ae7de2838a964b080c407d
Vasyl Kuznyak <vasyl.kuznyak@gmail.com>
2022-05-18 23:27:32 +0300

    done homework 3

--- src/app/app/app-routing.module.ts
+++ src/app/app/app-routing.module.ts
@@ -7,6 +7,8 @@ import {MainLayoutComponent} from "../layouts";
 const routes: Routes = [
   {
     path: '', component: MainLayoutComponent, children: [
+      {path: '', redirectTo: 'home', pathMatch: 'full'},
+      {path: 'home', loadChildren: () => import('../home/home.module').then(module => module.HomeModule)},
       {path: 'users', loadChildren: () => import('../user/user.module').then(module => module.UserModule)},
       {path: 'posts', loadChildren: () => import('../post/post.module').then(module => module.PostModule)},
       {path: 'comments', loadChildren: () => import('../comment/comment.module').then(module => module.CommentModule)}
